753252e8acb2e9067d2887046f92b2a210426951
Thomas Chung <thomas.chung@live.com>
2018-12-30 12:23:08 -0800

    Initial UserController for testing

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using CollabList.Model;
@@ -12,11 +14,37 @@ namespace CollabList.Controller
     {
 
         [HttpGet("{id}")]
-        public async Task<User> GetUser()
+        public async Task<ActionResult<User>> GetUser(Guid userId)
         {
-            AzureSqlConnectioProvider conn
+            AzureSqlConnectionProvider provider= new AzureSqlConnectionProvider();
 
-            using (SqlConnection connection = )
+            using (SqlConnection connection = provider.CreateConnection())
+            {
+                await connection.OpenAsync();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.prc_UserGet_1218";
+                    command.Parameters.Add(new SqlParameter("@userId", SqlDbType.UniqueIdentifier) { Value = userId} );
+                    
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        if (reader.Read())
+                        {
+                            Guid foundUserId = reader.GetGuid(reader.GetOrdinal("UserId"));
+                            string email = reader.GetString(reader.GetOrdinal("Email"));
+                            DateTime createdDateTime = reader.GetDateTime(reader.GetOrdinal("CreatedDateTime"));
+
+                            return new User(foundUserId, email, createdDateTime);
+                        }
+                        else
+                        {
+                            // Found no record return a 404
+                            return NotFound();
+                        }
+                    }
+                }
+            }
         }
     }
 }
